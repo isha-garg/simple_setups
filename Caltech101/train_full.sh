350528c94ef3c7a4481297961e86c1220b6751b4
isha-garg <garg.isha1991@gmail.com>
2017-04-09 15:47:43 +0800

    Add files via upload

--- Caltech101/train_full.sh
+++ Caltech101/train_full.sh
@@ -4,4 +4,14 @@ set -e
 TOOLS=./build/tools
 
 $TOOLS/caffe train \
-    --solver=models/caltech_isha/solver.prototxt --gpu=2 -weights models/bvlc_reference_caffenet/bvlc_reference_caffenet_copy1.caffemodel $@
+    --solver=examples/caltech_isha/caltech101_solver.prototxt --gpu=0,1 $@
+
+# reduce learning rate by factor of 10
+#$TOOLS/caffe train \
+#    --solver=examples/caltech_isha/cifar10_full_solver_lr1.prototxt \
+#    --snapshot=examples/caltech_isha/cifar10_full_iter_60000.solverstate.h5 $@
+
+# reduce learning rate by factor of 10
+#$TOOLS/caffe train \
+#    --solver=examples/caltech_isha/cifar10_full_solver_lr2.prototxt \
+#    --snapshot=examples/caltech_isha/cifar10_full_iter_65000.solverstate.h5 $@
